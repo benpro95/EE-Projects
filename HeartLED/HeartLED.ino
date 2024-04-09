c9fdab3642cd1e9ce9f63e22abea8435f514ef52
Ben Provenzano III <benprovenzano@gmail.com>
2024-04-08 21:35:57 -0400

    2024-04-08 21:35

--- HeartLED/HeartLED.ino
+++ HeartLED/HeartLED.ino
@@ -1,15 +1,32 @@
-#include <neotimer.h> 
+#include <BlockNot.h>   
 
-#define outPinHigh 12
+// LED driver globals
 #define outPinLow 2
-#define ledsCount 19
-uint8_t ledTranslate[] = {9,13,3,15,5,8,18,11,1,6,16,14,4,17,7,12,2,0,10,19};
+#define outPinHigh 12
+#define ledsCount 19 
+#define drawRate 75 // ms
+#define refreshRate 50 // us
+uint8_t blankRate = refreshRate / 2;
+BlockNot drawTimer(refreshRate, MICROSECONDS); 
+BlockNot blankingTimer(blankRate, MICROSECONDS); 
 uint8_t _upperHalf = (ledsCount / 2) + 1;
-Neotimer effectTimer = Neotimer(250);
-Neotimer ledRefresh = Neotimer(0.10);
-bool ledData[ledsCount + 1];
+uint8_t ledTranslate[] = {13,3,15,5,8,18,11,1,6,16,14,4,17,7,12,2,0,10,19,9};
+bool ledData[ledsCount + 1] = {0};
+// LED effect globals
+BlockNot effectTimer(drawRate, MILLISECONDS);
 uint8_t ledIncrement = 0;
 bool ledCountState = 0;
+
+void setup() {
+  //Serial.begin(115200);
+  // initialize LED output pins 
+  for(uint8_t _count = outPinLow; _count <= outPinHigh; _count++) {
+    pinMode(_count, OUTPUT);
+    digitalWrite(_count, LOW);
+  }
+  // initalize LED data array
+  clearLEDdata();
+}
 
 void clearLEDdata() {
   uint8_t _count;
@@ -18,52 +35,60 @@ void clearLEDdata() {
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  uint8_t _count;
-  // initialize LED output pins 
-  for(_count = outPinLow; _count <= outPinHigh; _count++) {
-    pinMode(_count, OUTPUT);
-    digitalWrite(_count, LOW);
-  }
-  // initalize LED data array
-  clearLEDdata();
+void showLEDdata() {
+ for(uint8_t _count = 0; _count <= ledsCount; _count++) {
+   Serial.print(ledData[_count]);
+   Serial.print(",");
+ }
+ Serial.print(ledIncrement);
+ Serial.println(" ");
 }
 
-void writeLED(uint8_t _ledin) {
+void writeLEDs() {
   bool _state = 0;
-  uint8_t _ledOuts = 0;  
-  uint8_t _stateCount = 0;
-  bool _ledStates[ledsCount + 1];
-  // translate to actual LED position
-  uint8_t _led = ledTranslate[_ledin];
-  // write LED state array
-  for(uint8_t _count = 0; _count <= ledsCount; _count++) {
-    if (_count == _led){ 
-      _ledStates[_count] = 1;   
-    } else {
-      _ledStates[_count] = 0; 
-    }
-  }
-  // toggle shift register
-  if (_led >= _upperHalf){
-    digitalWrite(outPinHigh, HIGH);
-    _stateCount = _upperHalf;
-  } else {
-    _stateCount = 0;
-    digitalWrite(outPinHigh, LOW);
-  }
-  // write out bank A (0-9) or bank B (10-19) LEDs
-  for(_ledOuts = outPinLow; _ledOuts <= outPinHigh - 1; _ledOuts++) {
-    _state = _ledStates[_stateCount];
-    digitalWrite(_ledOuts, _state);
-    _stateCount++;
+  uint8_t _ledOut = 0;  
+  uint8_t _statesIndex = 0;
+  bool _ledStates[ledsCount + 1] = {0};
+
+  if (drawTimer.TRIGGERED_ON_DURATION) {
+    // draw each LED one-at-a-time
+  	for(uint8_t _count = 0; _count <= ledsCount; _count++) {
+  	  // translate data to physical LED layout
+  	  uint8_t _led = ledTranslate[_count];
+  	  _ledStates[_led] = ledData[_count];
+  	  // toggle shift register
+  	  if (_led >= _upperHalf){
+  	    digitalWrite(outPinHigh, HIGH);
+  	    _statesIndex = _upperHalf;
+        } else {
+  	    _statesIndex = 0;
+  	    digitalWrite(outPinHigh, LOW);
+  	  }
+  	  // translate LED layout to actual I/O pin
+  	  for(_ledOut = outPinLow; _ledOut <= outPinHigh - 1; _ledOut++) {
+  	    _state = _ledStates[_statesIndex];
+  	    if (_led == _statesIndex) {
+         break;
+        }
+  	    _statesIndex++;
+  	  }
+  	  digitalWrite(_ledOut, _state);
+      // LED on-interval
+      blankingTimer.RESET;  
+      for (;;) {
+        readInputs();
+        if (blankingTimer.FIRST_TRIGGER) {
+          digitalWrite(_ledOut, 0);
+          break;
+        }
+      }
+  	}
   }
 }
 
 void effectTrailA() {
-  uint8_t _count;
   // add/remove values from array
+  ledData[0] = 1;
   if (ledCountState == 1) {
     ledData[ledIncrement] = 0;
     ledIncrement--;
@@ -78,14 +103,10 @@ void effectTrailA() {
   if (ledIncrement >= ledsCount) {
     ledData[ledIncrement] = 1;
     ledCountState = 1;
-    ledData[0] = 1;
-  } else {
-    ledData[0] = 0;
   }
 }
 
 void effectTrailB() {
- uint8_t _count;
  if (ledIncrement > ledsCount) {
    ledIncrement = 0;
    clearLEDdata();
@@ -95,27 +116,16 @@ void effectTrailB() {
  }
 }
 
-void showLEDdata() {
-  for(uint8_t _count = 0; _count <= ledsCount; _count++) {
-   Serial.print(ledData[_count]);
-   Serial.print(",");
-  }
-  Serial.print(ledIncrement);
-  Serial.println(" ");
+void readInputs() {
+  
 }
 
 void loop() {
-  // re-draw all LEDs
-  if(ledRefresh.repeat()){
-    for(uint8_t led = 0; led <= ledsCount; led++) {
-      if (ledData[led] == 1){
-        writeLED(led);
-      }
-    }
-  }
-  // update LEDs
-  if(effectTimer.repeat()){
+  readInputs();
+  if (effectTimer.TRIGGERED_ON_DURATION) {
+    // update LEDs
     effectTrailA();
     //showLEDdata();
   }
+  writeLEDs();
 }
